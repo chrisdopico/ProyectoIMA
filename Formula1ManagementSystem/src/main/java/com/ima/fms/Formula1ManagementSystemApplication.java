57b46b9d78affa47c3a66141631853cf49bcc42c
Juan Carlos Hernandez <jchernandez9606@gmail.com>
2021-12-19 14:25:58 +0100

    probando git

--- Formula1ManagementSystem/src/main/java/com/ima/fms/Formula1ManagementSystemApplication.java
+++ Formula1ManagementSystem/src/main/java/com/ima/fms/Formula1ManagementSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Formula1ManagementSystemApplication {
-
+	//prueba
 	public static void main(String[] args) {
 		SpringApplication.run(Formula1ManagementSystemApplication.class, args);
 	}
