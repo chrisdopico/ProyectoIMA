26a2ca1484f3495e60e59ebc3938c509adbf9982
Christian Dopico <christian.dopico@example.com>
2022-01-03 06:45:01 +0100

    Agregados scripts sql para llenar base de datos

---
+++ circuitos.sql
@@ -0,0 +1,29 @@
+INSERT INTO `formula1`.`t_circuitos` (`ciudad`, `curvas_lentas`, `curvas_medias`, `curvas_rapidas`, `longitud`, `nombre`, `numero_de_vueltas`, `pais`, `trazado`) 
+VALUES ('Spielberg', '2', '1', '1', '4.318', 'GP de Austria', '71', 'Austria', 'https://www.racingcircuits.info/assets/components/phpthumbof/cache/Red-Bull-Ring-2019.b3d6fe06fac602e0386be27f2932c884.png');
+
+INSERT INTO `formula1`.`t_circuitos` (`ciudad`, `curvas_lentas`, `curvas_medias`, `curvas_rapidas`, `longitud`, `nombre`, `numero_de_vueltas`, `pais`, `trazado`)
+VALUES ('Francorchamps','1','3','3','7.004','GP de Bélgica','44','Belgica','https://www.racingcircuits.info/assets/components/phpthumbof/cache/SpaF1-07.f8790385e69917207b1a12e00b639228.png');
+
+INSERT INTO `formula1`.`t_circuitos` (`ciudad`, `curvas_lentas`, `curvas_medias`, `curvas_rapidas`, `longitud`, `nombre`, `numero_de_vueltas`, `pais`, `trazado`)
+VALUES ('El Algarve','4','2','2','4.653','GP de Portugal','66','Portugal','https://www.racingcircuits.info/assets/components/phpthumbof/cache/Portimao-2021.36614d40a9b5c4073ab69b512393cdda.png');
+
+INSERT INTO `formula1`.`t_circuitos` (`ciudad`, `curvas_lentas`, `curvas_medias`, `curvas_rapidas`, `longitud`, `nombre`, `numero_de_vueltas`, `pais`, `trazado`)
+VALUES ('Barcelona','2','2','3','4.675','GP de España','66','España','https://www.racingcircuits.info/assets/components/phpthumbof/cache/Barcelona-2021.a4009a3e108e28ddcae739735d57e446.png');
+
+INSERT INTO `formula1`.`t_circuitos` (`ciudad`, `curvas_lentas`, `curvas_medias`, `curvas_rapidas`, `longitud`, `nombre`, `numero_de_vueltas`, `pais`, `trazado`)
+VALUES ('Montecarlo','2','1','2','3.337','GP de Mónaco','78','Mónaco','https://www.racingcircuits.info/assets/components/phpthumbof/cache/Monte-Carlo---2021.e867a7ce7c32e11be3f098fc1af532b8.png');
+
+INSERT INTO `formula1`.`t_circuitos` (`ciudad`, `curvas_lentas`, `curvas_medias`, `curvas_rapidas`, `longitud`, `nombre`, `numero_de_vueltas`, `pais`, `trazado`)
+VALUES ('Le Castellet','3','3','1','5.842','GP de Francia','53','Francia','https://www.racingcircuits.info/assets/components/phpthumbof/cache/Paul-Ricard-1A-19.ef687c3b39d9224bbfbb380fdfd49704.png');
+
+INSERT INTO `formula1`.`t_circuitos` (`ciudad`, `curvas_lentas`, `curvas_medias`, `curvas_rapidas`, `longitud`, `nombre`, `numero_de_vueltas`, `pais`, `trazado`)
+VALUES ('Zandvoort','1','1','3','4.259','GP de los Países Bajos','72','Países Bajos','https://www.racingcircuits.info/assets/components/phpthumbof/cache/Zandvoort20.b428501ee7d227d7663edc60354f37a0.png');
+
+INSERT INTO `formula1`.`t_circuitos` (`ciudad`, `curvas_lentas`, `curvas_medias`, `curvas_rapidas`, `longitud`, `nombre`, `numero_de_vueltas`, `pais`, `trazado`)
+VALUES ('Monza','2','2','1','5.793','GP de Italia','53','Italia','https://www.racingcircuits.info/assets/components/phpthumbof/cache/Monza2010.338442404079e95892772ec79cc64836.png');
+
+INSERT INTO `formula1`.`t_circuitos` (`ciudad`, `curvas_lentas`, `curvas_medias`, `curvas_rapidas`, `longitud`, `nombre`, `numero_de_vueltas`, `pais`, `trazado`)
+VALUES ('Montreal','2','3','2','4.361','GP de Canadá','70','Canadá','https://www.racingcircuits.info/assets/components/phpthumbof/cache/Montreal02.e6adc2ec9fb92e60b08655adc6fb4342.png');
+
+INSERT INTO `formula1`.`t_circuitos` (`ciudad`, `curvas_lentas`, `curvas_medias`, `curvas_rapidas`, `longitud`, `nombre`, `numero_de_vueltas`, `pais`, `trazado`)
+VALUES ('Ciudad de México','4','3','3','4.304','GP de México','71','México','https://www.racingcircuits.info/assets/components/phpthumbof/cache/Mexico-City-GP-Foro-Sol2020.47e2c55ea0fc645fb34b5885f04efda8.png');
---
+++ pilotos.sql
@@ -0,0 +1,17 @@
+INSERT INTO `formula1`.`t_pilotos` (`apellidos`, `dorsal`, `foto`, `nombre`, `pais`, `siglas`, `twitter`) 
+VALUES ('Räikkönen','7','https://www.formula1.com/content/fom-website/en/drivers/kimi-raikkonen/_jcr_content/image.img.1024.medium.jpg/1617101460196.jpg','Kimi','Finlandia','KR','https://twitter.com/Kimi7iceman');
+
+INSERT INTO `formula1`.`t_pilotos` (`apellidos`, `dorsal`, `foto`, `nombre`, `pais`, `siglas`, `twitter`) 
+VALUES ('Giovinazzi','99','https://www.formula1.com/content/fom-website/en/drivers/antonio-giovinazzi/_jcr_content/image.img.1024.medium.jpg/1617101458903.jpg','Antonio','Italia','AG','https://twitter.com/anto_giovinazzi');
+
+INSERT INTO `formula1`.`t_pilotos` (`apellidos`, `dorsal`, `foto`, `nombre`, `pais`, `siglas`, `twitter`) 
+VALUES ('Vettel','5','https://www.formula1.com/content/fom-website/en/drivers/sebastian-vettel/_jcr_content/image.img.1024.medium.jpg/1617101458077.jpg','Sebastian','Alemania','SV','https://twitter.com/sebvettelnews');
+
+INSERT INTO `formula1`.`t_pilotos` (`apellidos`, `dorsal`, `foto`, `nombre`, `pais`, `siglas`, `twitter`) 
+VALUES ('Stroll','18','https://www.formula1.com/content/fom-website/en/drivers/lance-stroll/_jcr_content/image.img.1024.medium.jpg/1617101447217.jpg','Lance','Canadá','LS','https://twitter.com/lance_stroll');
+
+INSERT INTO `formula1`.`t_pilotos` (`apellidos`, `dorsal`, `foto`, `nombre`, `pais`, `siglas`, `twitter`) 
+VALUES ('Ricciardo','3','https://www.formula1.com/content/fom-website/en/teams/McLaren/_jcr_content/logo.img.jpg/1515152760829.jpg','Daniel','Australia','DR','https://twitter.com/danielricciardo');
+
+INSERT INTO `formula1`.`t_pilotos` (`apellidos`, `dorsal`, `foto`, `nombre`, `pais`, `siglas`, `twitter`) 
+VALUES ('Norris','4','https://www.formula1.com/content/fom-website/en/drivers/lando-norris/_jcr_content/image.img.1024.medium.jpg/1617101457428.jpg','Lando','Reino Unido','LN','https://twitter.com/LandoNorris');
