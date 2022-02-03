86e119bb5d8777c38552c1d8b7a236a76ebc39a8
Christian Dopico <christian.dopico@example.com>
2022-02-04 00:41:35 +0100

    Agregada feature de votaciones

--- Formula1ManagementSystem/src/main/java/com/ima/fms/controller/VotacionFormController.java
+++ Formula1ManagementSystem/src/main/java/com/ima/fms/controller/VotacionFormController.java
@@ -11,9 +11,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.ima.fms.entity.Piloto;
+import com.ima.fms.entity.Votacion;
 import com.ima.fms.entity.VotacionForm;
 import com.ima.fms.service.PilotoService;
 import com.ima.fms.service.VotacionFormService;
+import com.ima.fms.service.VotacionService;
 
 
 @Controller
@@ -21,21 +23,57 @@ public class VotacionFormController {
 	
 	private VotacionFormService votacionFormService;
 	private PilotoService pilotoService;
+	private VotacionService votacionService;
 	
 	
 	
-	public VotacionFormController(VotacionFormService votacionFormService, PilotoService pilotoService) {
+	public VotacionFormController(VotacionFormService votacionFormService, PilotoService pilotoService, VotacionService votacionService) {
 		super();
 		this.votacionFormService = votacionFormService;
 		this.pilotoService = pilotoService;
+		this.votacionService = votacionService;
 	}
 
+	@PostMapping("/guardarVotacion")
+	public String guardarVotacion(@RequestParam("nombreVotacion") String nombreVotacion,
+			@RequestParam("correoVotacion") String correoVotacion,
+			@RequestParam("idPiloto") Long idPiloto) {
+		
+		List<Votacion> todasVotaciones = votacionService.getAllVotaciones();
+		List<String> nombreUsuarios = new ArrayList<String>();
+		List<String> correosUsuarios = new ArrayList<String>();
+		
+		for(Votacion votacion:todasVotaciones) {
+			nombreUsuarios.add(votacion.getNombreVotacion());
+			correosUsuarios.add(votacion.getCorreoVotacion());
+		}
+		
+		if(nombreUsuarios.contains(nombreVotacion)==false || correosUsuarios.contains(correoVotacion)==false) {
+			Votacion votacion = new Votacion(nombreVotacion, correoVotacion, idPiloto);
+			votacionService.saveVotacion(votacion);
+			return "redirect:/votacionesIndex";
+		}
+		else {
+			return "views_votacion/errorIndex";
+		}
+		
+		
+
+	}
+	
+	
+	
+	
+	
+	
+	
 
 	//
 	@GetMapping("/votacionesIndex")
 	public String listVotacionesFormIndex(Model model){
 		 List<VotacionForm> idPilotos= votacionFormService.getAllPilotosVotacion();
 		 List<Piloto> pilotosVotacion = new ArrayList<Piloto>();
+		 Votacion votacion = new Votacion();
 		 
 		 
 		 for(VotacionForm pilotoId:idPilotos) {
@@ -44,6 +82,7 @@ public class VotacionFormController {
 		 }
 		 
 		 model.addAttribute("listadoPilotos", pilotosVotacion);
+		 model.addAttribute("votacion",votacion);
 		 
 		 return "views_votacion/votacionForm";
 		 
@@ -82,7 +121,6 @@ public class VotacionFormController {
 	
 	
 	@PostMapping("/votaciones")
-	@ResponseBody
 	public String saveVotacionForm(@ModelAttribute("pilotoSeleccionado") Piloto pilotoSeleccionado) {
 		
 		List<VotacionForm> idPilotos= votacionFormService.getAllPilotosVotacion();
@@ -100,7 +138,7 @@ public class VotacionFormController {
 		}
 		
 		else {
-			return "Error";
+			return "views_votacion/error";
 		}
 		
 		
